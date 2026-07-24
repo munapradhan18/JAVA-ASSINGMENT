1e3b284b6a9befbf3711a57f65254f28a7fd0f7f
Muna Pradhan <munapradhanm97@gmail.com>
2026-07-25 00:12:02 +0530

    Update text2.java

--- text2.java
+++ text2.java
@@ -1,7 +1,7 @@
 class studentdetails {
     public static void main(String[] args) {
-        System.out.println("Name: subhankar patra");
-        System.out.println("Roll No: 64");
+        System.out.println("Name: muna pradhan");
+        System.out.println("Roll No: 25cse041");
         System.out.println("Branch: CSE");
     }
 }
